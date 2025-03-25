2766a4c9da62160e286ec38f320541d96c48a9b6
Lucas <lvksounds@gmail.com>
2025-03-25 15:01:39 -0300

    refactor: melhorando tratamento dos retornos e excessoes. Mudando a maneira que o service bate da API

--- app/services/weather_service.py
+++ app/services/weather_service.py
@@ -1,17 +1,24 @@
 import httpx #tipo httpClient no .net, da suporte para async e await
-import os 
+from app.config import API_KEY
 
 async def get_weather(city: str):
   async with httpx.AsyncClient() as client:
-    response = await client.get(
-      f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv('API_KEY')}&units=metric"
-    )
-    data = response.json()
-
-    if response.status_code != 200:
-      raise Exception(data["City not found."])
-  return {
-    "city": city,
-    "temperature": data["main"]["temp"],
-    "description": data["weather"][0]["description"]
-  }
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=pt"
+    try:
+      response = await client.get(url)
+      #response.raise_for_status() pegando erros em homolog
+      if response.status_code != 200:
+          return {"Erro" : "Não foi possivel consultar a cidade."}
+      data = response.json()
+      return {
+        "city": city,
+        "temperature": data["main"]["temp"],
+        "humidity":data["main"]["humidity"],
+        "description": data["weather"][0]["description"],
+      }
+    except httpx.HTTPStatusError as e:
+      return {"Erro" : f"Erro na requisição HTTP: {e}"}
+    except Exception as e:
+      return{"Erro" : f"Erro desconhecido."}
+    
+  

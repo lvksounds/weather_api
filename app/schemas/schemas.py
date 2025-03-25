3757f22c53e27cd87faedb31f1b289834c04f7c9
Lucas <lvksounds@gmail.com>
2025-03-25 15:07:55 -0300

    Feat: adicionando Dtos

---
+++ app/schemas/schemas.py
@@ -0,0 +1,19 @@
+# mesmo que dtos no .net - definindo dados de entrada e saida
+from pydantic import BaseModel
+from datetime import datetime 
+
+class PredictionBase(BaseModel):
+  city: str
+
+class CreatePrediction(PredictionBase):
+  pass
+
+class PredicitionResponse(PredictionBase):
+  id: int
+  temperature: float
+  humidity: float
+  description: str
+  date: datetime
+
+  class Config:
+    from_attributes = True

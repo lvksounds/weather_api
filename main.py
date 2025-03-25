0db7a6f9d0be98d6bff7d452a044e0a66d0cb412
Lucas <lvksounds@gmail.com>
2025-03-25 15:05:41 -0300

    fix: corrigindo a inserção das rotas no app. Adicionando tratamento para quando rodar o debug pelo uvicorn

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
-from app.routes import router
+from app.routers.prediction import router
+from app.infra.db_connection import engine, Base 
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Weather API", description="API para buscar e armazenar previsões do tempo", version="0.1.0")
-
 app.include_router(router)
 
 @app.get("/")
 def Home():
   return {"message": "API para Previsão do Tempo"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)

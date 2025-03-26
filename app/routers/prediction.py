cd5890776aec577839297195c4410d4a0737c5a3
Lucas <lvksounds@gmail.com>
2025-03-26 11:16:11 -0300

    Refactor: retirando logica e tratamento de dados e deixando só a chamada aos services

--- app/routers/prediction.py
+++ app/routers/prediction.py
@@ -1,80 +1,39 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.infra import db_connection
-from app.models.predictions import Prediction
-from app.schemas import schemas
-from app.services.weather_service import get_weather
-from app.helpers import dates_helper
 from typing import List, Optional
-from datetime import date, datetime
-from starlette.responses import JSONResponse
-from app.helpers.strings_helper import remove_accents 
+from app.infra import db_connection
+from app.schemas import schemas
+from app.services import prediction_service
 
 router = APIRouter(prefix="/predictions", tags=["Predictions"])
 
 @router.post("/")
 async def create_prediction(city: str, db: Session = Depends(db_connection.get_database)):
-  """
-  Busca a previsão do tempo para uma cidade e armazena no banco de dados.
-  """
-  city = remove_accents(city).lower()
-  try:
-    data = await get_weather(city)
-    if data.get("Erro"):
-      raise HTTPException(status_code=404, detail="Cidade não encontrada")
-
-    if "date" not in data: #só uma maneira preguiçosa de formatar essa data
-      data["date"] = date.today()
-    
-    prediction = Prediction(**data)
-    db.add(prediction)
-    db.commit()
-    db.refresh(prediction)
-    return JSONResponse(status_code=201, content={"detail": "Previsão criada com sucesso."})
-  except ValueError as ve:
-    return {"erro": str(ve)}
-  except Exception as e:
-    return {"erro": "Ocorreu um erro interno no servidor."}
-  
+    """
+    Busca a previsão do tempo para uma cidade e armazena no banco de dados.
+    """
+    return await prediction_service.create_prediction(city, db)
 
 @router.get("/", response_model=List[schemas.PredictionResponse])
-async def getAllPredictions(db: Session = Depends(db_connection.get_database)):
-  """
-  Retorna um lista com todas as previsões do tempo armazenadas no banco de dados.
-  """
-  return db.query(Prediction).all() 
+async def get_all_predictions(db: Session = Depends(db_connection.get_database)):
+    """
+    Retorna uma lista com todas as previsões armazenadas no banco de dados.
+    """
+    return prediction_service.get_all_predictions(db)
 
 @router.get("/filter", response_model=Optional[schemas.PredictionResponse])
-async def getPrediction(city: Optional[str] = None, date: Optional[str] = 
-                        None, db: Session = Depends(db_connection.get_database)):
-  """
-  Retorna uma previsão do tempo armazenada no banco de dados de acordo com a cidade e a data informadas.
-  formato dos inputs:
-  city: string
-  date: string no formato "DD/MM/YYYY"
-  """
-  
-  if city and date:
-    formatedDate = dates_helper.converStringToDate(date)
-    city = remove_accents(city).lower()
-    prediction = db.query(Prediction).filter(Prediction.city == city, Prediction.date == formatedDate).first()
-    if prediction is None:
-      raise HTTPException(status_code=404, detail="Previsão não encontrada.")
-    return prediction
-  elif city:
-     raise HTTPException(status_code=404, detail="Data não informada.")
-  elif date:
-     raise HTTPException(status_code=404, detail="Cidade não informada.")
-  
+async def get_prediction(city: Optional[str] = None, date: Optional[str] = None, db: Session = Depends(db_connection.get_database)):
+    """
+    Retorna uma previsão específica com base na cidade e na data informadas.\n
+    Formato dos parâmetros:\n
+    - city: str (obrigatório)\n
+    - date: str (obrigatório) no formato "dd/mm/aaaa"\n
+    """
+    return prediction_service.get_prediction_by_city_and_date(city, date, db)
+
 @router.delete("/delete/{id}")
-async def deletePrediction(id: int, db: Session = Depends(db_connection.get_database)):
-  """
-  Deleta uma previsão do tempo armazenada no banco de dados de acordo com o id informado.
-  Obtenha o id a partir do getPrediction ou do getAllPredictions.
-  """
-  prediction = db.query(Prediction).filter(Prediction.id == id).first()
-  if not prediction:
-        raise HTTPException(status_code=404, detail="Previsão não encontrada.")
-  db.delete(prediction)
-  db.commit()
-  return JSONResponse(status_code=200, content={"detail": "Previsão deletada com sucesso"})
+async def delete_prediction(id: int, db: Session = Depends(db_connection.get_database)):
+    """
+    Deleta uma previsão do tempo pelo ID.
+    """
+    return prediction_service.delete_prediction(id, db)

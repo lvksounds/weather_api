f9d741da6f48d2e44552db3a24cdb568fd8d2974
Lucas <lvksounds@gmail.com>
2025-03-26 11:16:51 -0300

    Feat/Refactor: refatorando e adicionando mais situações para testes

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from app.models.predictions import Base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db" # usando mesmo banco da produção
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -28,12 +28,18 @@ def override_get_db():
 
 app.dependency_overrides[get_database] = override_get_db
 
-# como nao estou usando um banco de teste, comentado pra nao criar muitos registros a toa
-# def test_create_prediction(client):
-#     response = client.post("/predictions/", params={"city": "São Jose dos Campos"})
-#     assert response.status_code == 201
-#     assert "detail" in response.json()
-#     assert response.json()["detail"] == "Previsão criada com sucesso."
+#como nao estou usando um banco de teste, comentado pra nao criar muitos registros a toa
+def test_create_prediction(client):
+    response = client.post("/predictions/", params={"city": "Itapemirim"})
+    assert response.status_code == 201
+    assert "detail" in response.json()
+    assert response.json()["detail"] == "Previsão criada com sucesso."
+
+def test_create_prediction_not_found(client):
+    response = client.post("/predictions/", params={"city": "aaooossiiejjs"})
+    assert response.status_code == 404
+    assert "detail" in response.json()
+    assert response.json()["detail"] == "Cidade não encontrada"
 
 def test_get_all_predictions(client):
     response = client.get("/predictions/")
@@ -41,23 +47,33 @@ def test_get_all_predictions(client):
     assert isinstance(response.json(), list)  # Verifica se a resposta é uma lista
 
 def test_get_prediction_by_city_and_date(client):
-    response = client.get("/predictions/filter", params={"city": "São Jose dos Campos", "date": "26/03/2025"})
+    response = client.get("/predictions/filter", params={"city": "Itapemirim", "date": "26/03/2025"})
     assert response.status_code == 200
     assert "city" in response.json()
-    assert response.json()["city"] == "sao jose dos campos"
+    assert response.json()["city"] == "itapemirim"
 
-def test_get_prediction_missing_city(client):
-    response = client.get("/predictions/filter", params={"date": "25/03/2025"})
+def test_get_prediction_by_city_and_date_not_found(client):
+    response = client.get("/predictions/filter", params={"city": "Paty do Alferes", "date": "26/03/2025"})
     assert response.status_code == 404
+    assert response.json()["detail"] == "Previsão não encontrada."
+
+def test_get_prediction_by_city_and_date_missing_city(client):
+    response = client.get("/predictions/filter", params={"date": "25/03/2025"})
+    assert response.status_code == 401
     assert response.json()["detail"] == "Cidade não informada."
 
-def test_get_prediction_missing_date(client):
-    response = client.get("/predictions/filter", params={"city": "São Jose dos Campos"})
-    assert response.status_code == 404
+def test_get_prediction_by_city_and_date_missing_date(client):
+    response = client.get("/predictions/filter", params={"city": "Itapemirim"})
+    assert response.status_code == 401
     assert response.json()["detail"] == "Data não informada."
 
 #mudar sempre o id
-# def test_delete_prediction(client):
-#     response = client.delete("/predictions/delete/2")
-#     assert response.status_code == 200
-#     assert response.json()["detail"] == "Previsão deletada com sucesso"
+def test_delete_prediction(client):
+    response = client.delete("/predictions/delete/9")
+    assert response.status_code == 200
+    assert response.json()["detail"] == "Previsão deletada com sucesso"
+
+def test_delete_prediction_not_found(client):
+    response = client.delete("/predictions/delete/9")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Previsão não encontrada."

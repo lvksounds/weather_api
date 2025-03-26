0cd627cf4f7e09c377490b3059db15c3506a284b
Lucas <lvksounds@gmail.com>
2025-03-26 10:03:46 -0300

    aaa

--- app/services/convert_date_service.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def converStringToDate(date: str) -> datetime:
-  date_obj = datetime.strptime(date, "%d/%m/%Y")
-  formated_date = date_obj.strftime("%Y-%m-%d 00:00:00.000000") # feio isso aqui
-  return formated_date
